4bf1fbc318a019cd9b31820c3fe4c7914bf7a683
Jeremy Iverson <jeremy@colectica.com>
2016-12-14 09:13:18 -0600

    tabs to spaces

--- src/CSharp/ColecticaSdkSamples/Program.cs
+++ src/CSharp/ColecticaSdkSamples/Program.cs
@@ -14,35 +14,35 @@ using ColecticaSdkSamples.Basic;
 
 namespace ConsoleApplication2
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			// It is helpful to set some default properties before
-			// working with the SDK's model. These two properties
-			// determine the default language and agency identifier
-			// for every item.
-			MultilingualString.CurrentCulture = "en-US";
-			VersionableBase.DefaultAgencyId = "example.org";
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // It is helpful to set some default properties before
+            // working with the SDK's model. These two properties
+            // determine the default language and agency identifier
+            // for every item.
+            MultilingualString.CurrentCulture = "en-US";
+            VersionableBase.DefaultAgencyId = "example.org";
 
-			// Call some sample method from the Intro class.
-			Intro intro = new Intro();
-			intro.BuildSomeDdiAndWriteToXml();
-			intro.LoadDdiAndCountSomeElements();
-			intro.QueryObjectModel();
+            // Call some sample method from the Intro class.
+            Intro intro = new Intro();
+            intro.BuildSomeDdiAndWriteToXml();
+            intro.LoadDdiAndCountSomeElements();
+            intro.QueryObjectModel();
 
             // Visit the graph that we made.
             var visitor = new ConsoleWriterVisitor();
             intro.Instance.Accept(visitor);
 
-			// Also, look around the RepositoryIntro class and the various
-			// classes in the Tools namespace.
+            // Also, look around the RepositoryIntro class and the various
+            // classes in the Tools namespace.
 
-			Console.WriteLine("Press any key to exit.");
-			Console.ReadLine();
-		}
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+        }
 
-	}
+    }
 
 
 }
